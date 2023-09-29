0cb7ad9fb58521be41378147ddabaa0db4933358
Aaron Ritter <aaron@sonderformat.llc>
2023-09-29 18:02:13 +0200

    refactor(data-source): remove users from example

--- examples/data-source/main.tf
+++ examples/data-source/main.tf
@@ -40,35 +40,3 @@ resource "fusionauth_application_role" "forum_user_role" {
   is_super_role  = false
   name           = "user"
 }
-
-resource "fusionauth_user" "forum-user1" {
-  email                    = "forum-user1@email.internal"
-  first_name               = "John"
-  last_name                = "Doe"
-  middle_name              = "William"
-  password_change_required = true
-  password                 = "%WLTvrsYELsyPqC^R7FMUNxt##VyDf6XaWk2R7!gS$oL76Ww"
-  username_status          = "ACTIVE"
-}
-
-resource "fusionauth_user" "forum-admin1" {
-  email                    = "forum-admin1@email.internal"
-  first_name               = "John"
-  last_name                = "Doe"
-  middle_name              = "William"
-  password_change_required = true
-  password                 = "@CfosPAVT3&hCzz5c^&#2F5BxNUY$X!@s!7Wx9bd6Yon54e3"
-  username_status          = "ACTIVE"
-}
-
-resource "fusionauth_registration" "forum-admin1-admin-role" {
-  user_id        = fusionauth_user.forum-admin1.id
-  application_id = fusionauth_application.forum.id
-  roles          = ["admin"]
-}
-
-resource "fusionauth_registration" "forum-user1-user-role" {
-  user_id        = fusionauth_user.forum-user1.id
-  application_id = fusionauth_application.forum.id
-  roles          = ["user"]
-}
